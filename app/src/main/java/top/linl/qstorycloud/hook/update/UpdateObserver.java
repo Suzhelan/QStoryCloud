78fd784c766794625683e931f900edf0a3dcee43
suzhelan <suzhelan@gmail.com>
2024-07-18 13:29:19 +0800

    fix:Download multiple times

--- app/src/main/java/top/linl/qstorycloud/hook/update/UpdateObserver.java
+++ app/src/main/java/top/linl/qstorycloud/hook/update/UpdateObserver.java
@@ -1,6 +1,7 @@
 package top.linl.qstorycloud.hook.update;
 
 import java.io.File;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 import io.reactivex.rxjava3.functions.Consumer;
 import top.linl.qstorycloud.db.LocalModuleInfoDAO;
@@ -10,14 +11,18 @@ import top.linl.qstorycloud.hook.PathTool;
 import top.linl.qstorycloud.hook.moduleloader.model.LocalModuleInfo;
 import top.linl.qstorycloud.hook.update.model.UpdateInfo;
 import top.linl.qstorycloud.hook.update.util.DownloadTask;
+import top.linl.qstorycloud.hook.util.ActivityTools;
 import top.linl.qstorycloud.hook.util.ToastTool;
 import top.linl.qstorycloud.log.QSLog;
+import top.linl.qstorycloud.util.TaskManager;
 
 /**
  * 更新观察者类
  */
 public class UpdateObserver implements Consumer<UpdateInfo> {
 
+    public AtomicBoolean isDownloading = new AtomicBoolean();
+    public AtomicBoolean isUpdateEnd = new AtomicBoolean();
 
     /**
      * 收到更新信息
@@ -25,6 +30,12 @@ public class UpdateObserver implements Consumer<UpdateInfo> {
     @Override
     public void accept(UpdateInfo updateInfo) {
         if (updateInfo.getUpdateUrl() == null) return;
+
+        //如果在下载中那不重复触发
+        if (isDownloading.get()) return;
+        //如果更新结束那不重复触发
+        if (isUpdateEnd.get()) return;
+
 
         UpdateInfo localUpdateInfo = UpdateInfoDAO.getLastUpdateInfo();
         LocalModuleInfo localModuleInfo = LocalModuleInfoDAO.getLastModuleInfo();
@@ -36,9 +47,10 @@ public class UpdateObserver implements Consumer<UpdateInfo> {
 
         //检测是否是首次使用或者有云端更新
         //本地模块信息为空说明是第一次使用 || 本地模块版本号小于最新云端版本号
-        if (localModuleInfo == null || localModuleInfo.getModuleVersionCode() != updateInfo.getLatestVersionCode()) {
+        if (localModuleInfo == null || localModuleInfo.getModuleVersionCode() < updateInfo.getLatestVersionCode()) {
             sendUpdateTask(updateInfo);
         }
+
         //模块文件不存在也进行更新
         else if (!new File(localModuleInfo.getModuleApkPath()).exists()) {
             sendUpdateTask(updateInfo);
@@ -50,7 +62,9 @@ public class UpdateObserver implements Consumer<UpdateInfo> {
      * 发送更新任务
      */
     public void sendUpdateTask(UpdateInfo updateInfo) {
+        isDownloading.set(true);
         try {
+
             ToastTool.show("QStory-云更新 开始执行更新任务");
             //下载
             String downloadPath = PathTool.getDataPath() + "/" + updateInfo.getLatestVersionName();
@@ -66,10 +80,16 @@ public class UpdateObserver implements Consumer<UpdateInfo> {
             //插入模块更新信息
             LocalModuleInfoDAO.insertModuleInfo(localModuleInfo);
             //数据库写入完成
-            ToastTool.show("更新任务执行成功,请重启QQ");
+            ToastTool.show("更新任务执行成功,重启QQ");
+
+            isUpdateEnd.set(true);
+            TaskManager.addDelayTask(() -> {
+                ActivityTools.killAppProcess(HookEnv.getHostAppContext());
+            }, 3000);
         } catch (Exception e) {
             ToastTool.show("更新失败：" + e);
             QSLog.e("更新失败", e);
         }
+        isDownloading.set(false);
     }
 }
--- app/src/main/java/top/linl/qstorycloud/util/TaskManager.java
+++ app/src/main/java/top/linl/qstorycloud/util/TaskManager.java
@@ -9,4 +9,10 @@ public class TaskManager {
         new Handler(Looper.getMainLooper())
                 .post(runnable);
     }
+    //添加延时任务
+    public static void addDelayTask(Runnable runnable, long delay)
+    {
+        new Handler(Looper.getMainLooper())
+                .postDelayed(runnable, delay);
+    }
 }
